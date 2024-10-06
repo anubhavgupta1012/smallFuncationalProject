c3a1a0877009f646a182413732a283fbf444dea8
Dheeraj <anubhavgupta1012@gmail.com>
2024-10-07 02:56:47 +0530

    backbone created

---
+++ src/main/java/com/functions/SmallFunctionalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.functions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallFunctionalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmallFunctionalProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/functions/SmallFunctionalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.functions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmallFunctionalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
